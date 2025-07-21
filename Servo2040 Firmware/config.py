3f0340d1e21f75069b31daff6bdbe863436e68f6
JacobLujanac <lujanac1026@gmail.com>
2025-07-21 16:36:34 -0400

    Update config.py

--- Servo2040 Firmware/config.py
+++ Servo2040 Firmware/config.py
@@ -1,8 +1,9 @@
 # Servo2040 Side Configurations
 
-LEG_NAMES = ["RB","LB","RM","LM","RF","LF"]  # Expand later
+LEG_NAMES = ["RB","LB","RM","LM","RF","LF"]
 
-JOINT_OFFSETS = {
+
+GLOBAL_OFFSETS = {
     "Hip": {
         "RF": 45, "RM": 0, "RB": -45,
         "LF": 135, "LM": 180, "LB": -135
@@ -10,6 +11,22 @@ JOINT_OFFSETS = {
     "Knee": 0,
     "Ankle": 0
 }
+
+#Offset from Installation
+JOINT_OFFSETS = {
+    "Hip": {
+        "RF": -5, "RM": -3, "RB": -3,
+        "LF": -15, "LM": 0, "LB": -6
+    },
+    "Knee": {
+        "RF": 2, "RM": 5, "RB": -3,
+        "LF": 6,"LM": -15, "LB": 0
+    },
+   "Ankle": {
+    "RF": -126, "RM": -95, "RB": -103,
+    "LF": -100,"LM": -113, "LB": -100
+    }
+} 
 
 ROM_LIMITS = {
     "Hip": (-90, 90),
